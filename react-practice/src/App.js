ce6dd5ff1bffe989c0547d49471a430019ae92c8
Brian Hart <brianhart@dhcp-v088-235.mobile.uci.edu>
2017-11-23 01:33:05 -0800

    initial commit

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './Components/Welcome';
+import Goodbye from './Components/Goodbye';
 
 class App extends Component {
+  
   render() {
     return (
+      <div>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">My React Practice</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          This is a practice react app.
+        </p>        
       </div>
-    );
-  }
-}
+      <div>
+        <Welcome />
+      </div>
+      <div>
+        <Goodbye />
+      </div>
+      </div>
+
+    )
+  }}
 
 export default App;
---
+++ react-practice/src/Components/Goodbye/Goodbye.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Goodbye = () =>
+        <div>
+        <h1>This is fun, but I should get some sleep. </h1>
+        </div>
+    
+
+export default Goodbye;
